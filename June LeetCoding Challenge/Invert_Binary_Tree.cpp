0bee8915b55fcf467433304e81f9fece5bcf14fb
Wesley <weil0819@gmail.com>
2020-06-02 11:18:07 +1000

    add Jun 1 challenge

--- June LeetCoding Challenge/Invert_Binary_Tree.cpp
+++ June LeetCoding Challenge/Invert_Binary_Tree.cpp
@@ -30,12 +30,42 @@ Output:
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    // Recursive method
+    /*
     TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) return NULL;
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+    */
+    
+    // Iterative method -- BFS -- queue<TreeNode*>
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) return NULL;
         
+        queue<TreeNode*> Q;
+        Q.push(root);
+        while(!Q.empty()) {
+            TreeNode* node = Q.front();
+            Q.pop();
+            
+            // push left & right
+            if(node->left) Q.push(node->left);
+            if(node->right) Q.push(node->right);
+            
+            // swap left and right
+            swap(node->left, node->right);
+        }
+        return root;
     }
 };
